f001542add5db423539a71224524d7161f48f07f
JoeVanCamp <vancampjoe@gmail.com>
2022-02-28 10:31:19 -0800

    Complete ForagerBee and HoneyMakerBee subclasses

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,19 @@
 var ForagerBee = function() {
+  // Call Bee class
+  Bee.call(this);
+  // Create some unique traits
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+// Set prototype
+ForagerBee.prototype = Object.create(Bee.prototype);
+// Reset constructor
+ForagerBee.prototype.constructor = ForagerBee;
+
+// Create a forage method for ForagerBee
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+};
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,24 @@
 var HoneyMakerBee = function() {
+  // Call Bee class
+  Bee.call(this);
+  // Create a few unique traits to HoneyMakers
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+// Set the prototype
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+
+// Reset the constructor
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+// makeHoney method for HoneyMakerBee
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+// giveHoney methof for HoneyMakerBee
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
