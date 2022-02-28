6363c61e9b8f1c2af5109dad3b9fbc6d59f2db0b
JoeVanCamp <vancampjoe@gmail.com>
2022-02-28 10:20:03 -0800

    Complete Bee subclass

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,16 @@
+// Create Bee class
 var Bee = function() {
+  // Call the super class 'Grub'
+  Grub.call(this);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
+
+
 };
+
+// Set Bee methods to inherit Grub methods
+// Reset Bee's contructor to not be overwritten
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
 
