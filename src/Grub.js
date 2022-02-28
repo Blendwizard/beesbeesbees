f4868498d08d6768d1597f4227a5cb30f053199d
JoeVanCamp <vancampjoe@gmail.com>
2022-02-28 10:05:59 -0800

    Complete Grub class

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,18 @@
+
+// Grub Super Class
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+
 };
 
+
+// Create an eat method for our Grub class
+Grub.prototype.eat = function(food) {
+  if (food === this.food) {
+    console.log(`Mmmm I love ${food}.`);
+  } else {
+    console.log(`I don't like ${food}.`);
+  }
+};
